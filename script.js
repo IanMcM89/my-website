08e0261b0d1d13c0ff22778f4dcde5b6887a0816
Ian <ianmcm.89@gmail.com>
2021-07-25 15:18:06 -0400

    Social media icons connected and functional. Header tab functionality in testing phase.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-/*
-
-const h1 = document.createElement('h1');
-h1.textContent = "IAN";
-document.querySelector('body').appendChild(h1)
-
-*/
+function homeButton () {
+    const divContent = document.getElementsByClassName("content");
+        
+        const h2 = document.createElement('h2');
+        h2.textContent = "Ian";
+        const h3 = document.createElement('h3');
+        h3.textContent = "Programming Trainee";
+        const p1 = document.createElement('p');
+        p1.textContent = "I'm currently enrolled in Flatiron School's Software-Engineering flex program, learning how to code from their brightest minds.";
+        const p2 = document.createElement('p');
+        p2.textContent = "To see more about me, click the icons below.";
+    
+        divContent[0].appendChild(h2);
+        divContent[0].appendChild(h3);
+        divContent[0].appendChild(p1);
+        divContent[0].appendChild(p2);
+}
